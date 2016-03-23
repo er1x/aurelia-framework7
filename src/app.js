c96189e6ada8614c62b1fad8f4c8550fe66481d9
Emilio J Grande <emilio@jsjutsu.com>
2016-03-23 21:23:29 +0100

    Add routing and some markup to todo list

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-export default class App{
-  
+import 'framework7'
+
+export default class App {
+  constructor () {
+    new window.Framework7({
+      router: false
+    })
+  }
+
+  configureRouter (config, router) {
+    config.map([
+      { route: ['', 'todos'], name: 'todos', moduleId: './todos', nav: true, title: 'Todos' },
+      { route: 'about', name: 'about', moduleId: './about', nav: true, title: 'About' }
+    ])
+
+    this.router = router
+  }
 }
